d691abb2d3184766a4394a1ecd844aff70c485d7
GabrielRazafimahatratra <fifalianagabrielrfg@gmail.com>
2024-04-09 18:39:15 +0300

    feat: create caissiers folder

--- src/caissiers/caissiers.module.ts
+++ src/caissiers/caissiers.module.ts
@@ -1,3 +1,10 @@
+import { Module } from "@nestjs/common";
+import { CaissiersController } from "./caissiers.controller";
+import { CaissiersService } from "./caissiers.service";
+import { PrismaService } from "prisma/prisma.service";
 
-import { CaissiersController } from './caissiers.controller';
-import { CaissiersService } from './caissiers.service';
+@Module({
+    controllers: [CaissiersController],
+    providers: [CaissiersService, PrismaService]
+})
+export class CaissiersModule{}
