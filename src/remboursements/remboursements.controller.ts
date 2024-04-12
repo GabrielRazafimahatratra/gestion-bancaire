1cd3e343d8b4de32b30eff78c607cdacabe8438e
GabrielRazafimahatratra <fifalianagabrielrfg@gmail.com>
2024-04-13 01:42:09 +0300

    feat: add reimboursements controller

--- src/remboursements/remboursements.controller.ts
+++ src/remboursements/remboursements.controller.ts
@@ -1,4 +1,34 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Patch, Post } from '@nestjs/common';
+import { RemboursementsService } from './remboursements.service';
+import { CreateRemboursementsDto } from './dto/create-remboursements.dto';
+import { UpdateRemboursementsDto } from './dto/update-remboursement.dto';
 
 @Controller('remboursements')
-export class RemboursementsController {}
+export class RemboursementsController {
+    constructor(private readonly remboursementService: RemboursementsService) {}
+
+    @Post()
+    async createRemboursement(@Body() createRemboursement: CreateRemboursementsDto){
+        return this.remboursementService.createRemboursement(createRemboursement);
+    }
+
+    @Get()
+    async findAllRemboursement() {
+        return this.remboursementService.findAllRemboursements();
+    }
+
+    @Get(':id')
+    async findOneRemboursement(@Param('id') idRemboursement: string) {
+        return this.remboursementService.findOneRemboursemen(idRemboursement);
+    }
+
+    @Patch(':id')
+    async updateRemboursement(@Param('id') idRemboursement: string, @Body() updateRemboursement: UpdateRemboursementsDto) {
+        return this.remboursementService.updateRemboursement(idRemboursement, updateRemboursement);
+    }
+
+    @Delete(':id')
+    async deleteRemboursement(@Param('id') idRemboursement: string) {
+        return this.remboursementService.deleteRemboursement(idRemboursement);
+    }
+}
