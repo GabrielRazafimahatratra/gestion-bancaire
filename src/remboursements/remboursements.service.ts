702c83e4f9be79115945c0204509097b3ec98813
GabrielRazafimahatratra <fifalianagabrielrfg@gmail.com>
2024-04-12 23:17:57 +0300

    feat(remboursement.service): add CRUD functionnalities

--- src/remboursements/remboursements.service.ts
+++ src/remboursements/remboursements.service.ts
@@ -1,4 +1,56 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaService } from 'prisma/prisma.service';
+import { CreateRemboursementsDto } from './dto/create-remboursements.dto';
+import { UpdateRemboursementsDto } from './dto/update-remboursement.dto';
 
 @Injectable()
-export class RemboursementsService {}
+export class RemboursementsService {
+    constructor(private readonly prisma: PrismaService) {}
+
+    async createRemboursement(createRemboursement: CreateRemboursementsDto) {
+        const remboursement = await this.prisma.remboursementPret.create({
+            data: createRemboursement
+        });
+
+        const remboursementToJSONType = JSON.stringify(remboursement);
+
+        return remboursementToJSONType;
+    }
+
+    async findAllRemboursements() {
+        const remboursements = await this.prisma.remboursementPret.findMany();
+
+        return remboursements;
+    }
+
+    async findOneRemboursemen(idRemboursement: string) {
+        const remboursement = await this.prisma.remboursementPret.findUnique({
+            where: {idRemboursement}
+        });
+
+        const remboursementToJSONType = JSON.stringify(remboursement);
+
+        return remboursementToJSONType;
+    }
+
+    async updateRemboursement(idRemboursement: string, updateRemboursement: UpdateRemboursementsDto) {
+        const remboursement = await this.prisma.remboursementPret.update({
+            where: {idRemboursement},
+            data: updateRemboursement
+        });
+
+        const remboursementToJSONType = JSON.stringify(remboursement);
+
+        return remboursementToJSONType;
+    }
+    
+    async deleteRemboursement(idRemboursement: string) {
+        const remboursement =await this.prisma.remboursementPret.delete({
+            where: {idRemboursement}
+        });
+
+        const remboursementToJSONType = JSON.stringify(remboursement);
+
+        return remboursementToJSONType;
+    }
+}
