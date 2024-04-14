c8a57b7357093493f8f5b95312bee3e27336c708
GabrielRazafimahatratra <fifalianagabrielrfg@gmail.com>
2024-04-14 13:00:24 +0300

    feat(virements): add CRUD functionnalities

--- src/virements/virements.controller.ts
+++ src/virements/virements.controller.ts
@@ -1,4 +1,34 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Patch, Post } from '@nestjs/common';
+import { VirementsService } from './virements.service';
+import { UpdateVirementsDtos } from './dtos/update-virements.dtos';
 
 @Controller('virements')
-export class VirementsController {}
+export class VirementsController {
+
+    constructor( private readonly virementService: VirementsService) {}
+
+    @Post()
+    async createVersement(@Body() createVersement: CreateVersementsDto) {
+        return this.virementService.createVersement(createVersement); 
+    }
+
+    @Get()
+    async findAllRetraits() {
+        return this.virementService.findAllVirements();
+    }
+
+    @Get(':id')
+    findOneRetrait(@Param('id') numeroVirement: string) {
+        return this.virementService.findOneVirement(numeroVirement);
+    }
+
+    @Patch(':id')
+    updateRetrait(@Param('id') numeroVirement: string, @Body() updateVirement: UpdateVirementsDtos) {
+        return this.virementService.updateVirement(numeroVirement, updateVirement);
+    }
+
+    @Delete(':id')
+    deleteRetrait(@Param('id') numeroVirement: string) {
+        return this.virementService.deleteVirement(numeroVirement);
+    }
+}
--- src/virements/virements.service.ts
+++ src/virements/virements.service.ts
@@ -1,4 +1,57 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaService } from 'prisma/prisma.service';
+import { CreateVirementsDto } from './dtos/create-virements.dto';
+import { UpdateVirementsDtos } from './dtos/update-virements.dtos';
 
 @Injectable()
-export class VirementsService {}
+export class VirementsService {
+
+    constructor( private readonly prisma: PrismaService) {}
+
+    async createVersement(createVirement: CreateVirementsDto) {
+        const virement = await this.prisma.virement.create({
+            data: createVirement
+        });
+        
+        const virementToJSONType = JSON.stringify(virement);
+
+        return virementToJSONType;
+    }
+
+    async findAllVirements() {
+        const virement = await this.prisma.virement.findMany();
+        const virementToJSONType = JSON.stringify(virement);
+
+        return virementToJSONType;
+    }
+
+    async findOneVirement(numeroVirement: string) {
+        const virement = await this.prisma.virement.findUnique({
+            where: {numeroVirement: numeroVirement}
+        });
+        const virementToJSONType = JSON.stringify(virement);
+
+        return virementToJSONType;
+    }
+
+    async updateVirement(numeroVirement: string, updateVirement: UpdateVirementsDtos) {
+        const virement = await this.prisma.virement.update({
+            where: {numeroVirement: numeroVirement},
+            data: updateVirement
+        });
+
+        const virementToJSONType = JSON.stringify(virement);
+
+        return virementToJSONType;
+    }
+
+    async deleteVirement(numeroVirement: string) {
+        const virement = await this.prisma.virement.delete({
+            where: {numeroVirement: numeroVirement}
+        });
+        
+        const virementToJSONType = JSON.stringify(virement);
+
+        return virementToJSONType;
+    }
+}
