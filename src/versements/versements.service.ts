8462300cffc0cb53de8b624071d64088790fd8ed
GabrielRazafimahatratra <fifalianagabrielrfg@gmail.com>
2024-04-14 11:54:02 +0300

    feat(versements): add CRUD functionnalities

--- src/versements/versements.service.ts
+++ src/versements/versements.service.ts
@@ -1,4 +1,57 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaService } from 'prisma/prisma.service';
+import { CreateVersementsDto } from './dtos/create-versements.dto';
+import { UpdateVersementsDto } from './dtos/update-versements.dto';
 
 @Injectable()
-export class VersementsService {}
+export class VersementsService {
+
+    constructor( private readonly prisma: PrismaService) {}
+
+    async createVersement(createVersement: CreateVersementsDto) {
+        const versement = await this.prisma.versement.create({
+            data: createVersement
+        });
+        
+        const versementToJSONType = JSON.stringify(versement);
+
+        return versementToJSONType;
+    }
+
+    async findAllVersements() {
+        const versement = await this.prisma.versement.findMany();
+        const versementToJSONType = JSON.stringify(versement);
+
+        return versementToJSONType;
+    }
+
+    async findOneVersement(numeroVersement: string) {
+        const versement = await this.prisma.versement.findUnique({
+            where: {numeroVersement: numeroVersement}
+        });
+        const versementToJSONType = JSON.stringify(versement);
+
+        return versementToJSONType;
+    }
+
+    async updateVersement(numeroVersement: string, updateVersement: UpdateVersementsDto) {
+        const versement = await this.prisma.versement.update({
+            where: {numeroVersement: numeroVersement},
+            data: updateVersement
+        });
+
+        const versementToJSONType = JSON.stringify(versement);
+
+        return versementToJSONType;
+    }
+
+    async deleteVersement(numeroVersement: string) {
+        const versement = await this.prisma.versement.delete({
+            where: {numeroVersement: numeroVersement}
+        });
+        
+        const versementToJSONType = JSON.stringify(versement);
+
+        return versementToJSONType;
+    }
+}
