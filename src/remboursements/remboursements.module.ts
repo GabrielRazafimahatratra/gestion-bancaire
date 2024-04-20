fcac178e4a93e3eb378951f8eca34ea373f42e14
GabrielRazafimahatratra <fifalianagabrielrfg@gmail.com>
2024-04-20 21:02:24 +0300

    fix: import of MyEmailService and MailerService

--- src/remboursements/remboursements.module.ts
+++ src/remboursements/remboursements.module.ts
@@ -4,10 +4,12 @@ import { RemboursementsController } from './remboursements.controller';
 import { RemboursementsService } from './remboursements.service';
 import { PrismaService } from 'prisma/prisma.service';
 import { HistoriquesService } from 'src/historiques/historiques.service';
+import { MyemailService } from 'src/myemail/myemail.service';
+import { MailerService } from 'src/mailer/mailer.service';
 
 
 @Module({
-    providers: [RemboursementsService, PrismaService, HistoriquesService],
+    providers: [RemboursementsService, PrismaService, HistoriquesService, MyemailService, MailerService],
     controllers: [RemboursementsController]
 })
 export class RemboursementModule {}
--- src/retraits/retraits.module.ts
+++ src/retraits/retraits.module.ts
@@ -4,9 +4,11 @@ import { RetraitsController } from './retraits.controller';
 import { RetraitsService } from './retraits.service';
 import { PrismaService } from 'prisma/prisma.service';
 import { HistoriquesService } from 'src/historiques/historiques.service';
+import { MyemailService } from 'src/myemail/myemail.service';
+import { MailerService } from 'src/mailer/mailer.service';
 
 @Module({
-    providers: [RetraitsService, PrismaService, HistoriquesService],
+    providers: [RetraitsService, PrismaService, HistoriquesService, MyemailService, MailerService],
     controllers: [RetraitsController]
 })
 export class RetraitsModule {}
